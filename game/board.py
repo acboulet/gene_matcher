7cc3ee4e4f18f20ffa090cc4cff95ade69543b71
Your Name <acboulet@outlook.com>
2021-07-14 21:37:00 -0600

    updating UML

--- game/board.py
+++ game/board.py
@@ -1,7 +1,8 @@
 from random import randint
 
+
 class GameBoard:
-    
+
     # Protected variable to store Game_board
     game_board = []
 
@@ -31,8 +32,8 @@ class GameBoard:
     """
     @staticmethod
     def random_base() -> str:
-        bases =["A", "C", "G", "T"]
-        rand_int = randint(0,3)
+        bases = ["A", "C", "G", "T"]
+        rand_int = randint(0, 3)
         return bases[rand_int]
     # check if move was successful
 
@@ -44,14 +45,16 @@ class GameBoard:
     Return:
         List containing codons for [0] horizontal and [1] vertical options.
     """
+
     def possible_codons(self, player_choice: list([int, int])) -> list([str, str]):
         # TODO currently it does not ask the pieces to switch
-        possible_codons = [None,None] # hor, vert
+        possible_codons = [None, None]  # hor, vert
         row, col = player_choice
         # If the col value is left/right then there is no horizontal option
         if col != 1 and col != 8:
             possible_row = self.game_board[row-1]
-            possible_codons[0] = possible_row[col-2] + possible_row[col-1] + possible_row[col]
+            possible_codons[0] = possible_row[col-2] + \
+                possible_row[col-1] + possible_row[col]
         # If the row value is top or bottom, then there is no vertical option
         if row != 1 and row != 8:
             first = self.game_board[row-2][col-1]
@@ -59,7 +62,6 @@ class GameBoard:
             third = self.game_board[row][col-1]
             possible_codons[1] = first + second + third
         return possible_codons
-
 
     """
     Purpose:
@@ -73,20 +75,20 @@ class GameBoard:
             [2] : [int, int] for location of second base
             [3] : [int, int] for location of third base
     """
+
     def possible_codon_v2(self, player_choices: list([int, int])):
         row_index = player_choices[0] - 1
         col_index = player_choices[1] - 1
-        
 
         codons = []
-        
+
         # move along the row, and grab each three-base codon
         # if reaching the end of the board (col = 6) or user choice then stop
-        col = col_index - 2 # -2 to find starting base
+        col = col_index - 2  # -2 to find starting base
         # If more than index 6 (column 7) then cannot grab 3 bases
         while col < 6 and col <= col_index:
             if col >= 0:
-                #[ codon, h:v1, h:v2, h:v3]
+                # [ codon, h:v1, h:v2, h:v3]
                 codon = []
                 first = self.game_board[row_index][col]
                 second = self.game_board[row_index][col+1]
@@ -101,10 +103,10 @@ class GameBoard:
 
          # move down the column, and grab each three-base codon
         # if reaching the end of the board (row = 6) or user choice then stop
-        row = row_index -2 
+        row = row_index - 2
         while row < 6 and row <= row_index:
             if row >= 0:
-                #[ codon, h:v1, h:v2, h:v3]
+                # [ codon, h:v1, h:v2, h:v3]
                 codon = []
                 first = self.game_board[row][col_index]
                 second = self.game_board[row + 1][col_index]
@@ -115,8 +117,8 @@ class GameBoard:
                 codon.append([row + 2, col_index + 1])
                 codon.append([row + 3, col_index + 1])
                 codons.append(codon)
-            row += 1    
-        
+            row += 1
+
         return codons
 
     """
@@ -134,6 +136,7 @@ class GameBoard:
             output += "\n"
         return output
 
+
 if __name__ == "__main__":
     test_board = GameBoard()
     # test_board.new_board()
@@ -141,11 +144,11 @@ if __name__ == "__main__":
 
     # Test of possible_codons()
     # First test is a corner which should yield no results
-    result = test_board.possible_codons([1,1])
+    result = test_board.possible_codons([1, 1])
     if result != [None, None]:
         print("possible_codon selection on corner does yield None, None")
         print(result)
-    
+
     # # Print statements because of randomly generated table
     # print("Test grabbing row 1 and col 4, should yield no vertical value:")
     # result = test_board.possible_codons([1,4])
@@ -158,28 +161,25 @@ if __name__ == "__main__":
     # print(result)
     # print("")
 
-    
     # Print statements testing possible_codon_helper
     print("Testing grabbing from center with v2")
-    result = test_board.possible_codon_v2([4,4])
+    result = test_board.possible_codon_v2([4, 4])
     print(result)
     print("Testing grabbing from left with v2")
-    result = test_board.possible_codon_v2([4,1])
+    result = test_board.possible_codon_v2([4, 1])
     print(result)
     print("Testing grabbing from right with v2")
-    result = test_board.possible_codon_v2([4,8])
+    result = test_board.possible_codon_v2([4, 8])
     print(result)
     print("Testing grabbing from top with v2")
-    result = test_board.possible_codon_v2([1,4])
+    result = test_board.possible_codon_v2([1, 4])
     print(result)
     print("Testing grabbing from bottom with v2")
-    result = test_board.possible_codon_v2([8,4])
+    result = test_board.possible_codon_v2([8, 4])
     print(result)
     print("Testing grabbing from top corner with v2")
-    result = test_board.possible_codon_v2([1,1])
+    result = test_board.possible_codon_v2([1, 1])
     print(result)
     print("Testing grabbing from bottom with v2")
-    result = test_board.possible_codon_v2([8,8])
+    result = test_board.possible_codon_v2([8, 8])
     print(result)
-    
-    
